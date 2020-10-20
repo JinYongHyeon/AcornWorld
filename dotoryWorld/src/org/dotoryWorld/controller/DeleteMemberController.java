a957b4065750f4872d1bcdbfa1c666b74d904f20
yulhwi6 <yulhwi6@gmail.com>
2020-10-20 14:26:40 +0900

    참조

--- dotoryWorld/src/org/dotoryWorld/controller/DeleteMemberController.java
+++ dotoryWorld/src/org/dotoryWorld/controller/DeleteMemberController.java
@@ -11,22 +11,7 @@ public class DeleteMemberController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		/*
-		HttpSession session = request.getSession();	
-		String id=request.getParameter("id");
-		MemberDAO.getInstance().deleteMember(id);
-            session.invalidate();
-            return "redirect:views/member/delete-result.jsp";
-            */
-           
-		/*
-			String id=request.getParameter("id");
-            String password=request.getParameter("password");
-            MemberVO vo=MemberDAO.getInstance().deleteMember(password);
-            if(vo!==)
-            
-			return "redirect:front?command=delete-confirm-fail";
-			*/
+		
     		HttpSession session = request.getSession(false);  // false를 써줘야 session이 새로 안 생기고 기존에 있던 session 에서 mvo를 가져옴	
     		MemberVO mvo =(MemberVO) session.getAttribute("mvo");
     		String password=mvo.getPassword();  // session 에서 가져온 password
